5046d4cb9d202f709bb68a95dc702954afd873be
Oleg Y <oleg.yakimets@gmail.com>
2013-11-21 16:02:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IbabaiP::Application.config.secret_key_base = 'aa0d36be82d4235c2177cfbb6c5e4cacb9ee715a212bee88bb0105938e28cf160989cc6ac110d8571f8058b1d612365bbc9364bf9440ee956ecc7d115ee0dc36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IbabaiP::Application.config.session_store :cookie_store, key: '_ibabai_p_session'
